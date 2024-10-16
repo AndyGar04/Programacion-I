6421aa5cc18be8265bfba26284ebcf0724502b0f
AndyGarcia <andygarciaprost2004@gmail.com>
2024-10-16 17:54:35 -0300

    Tp7

--- TpNro7/Ejercicio7Tp7/classEmpleado.py
+++
@@ -1,5 +0,0 @@
-"""
-Cierta empresa requiere una aplicación informática para administrar los datos de su
-personal, del cual se conoce: número de DNI, nombre, apellido y fecha de ingreso.
-Existen dos categorías de empleados: con salario fijo y a comisión
-"""
