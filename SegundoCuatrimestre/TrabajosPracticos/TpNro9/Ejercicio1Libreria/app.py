6fd0ff045b4b039558535e38287b8fec0af9f9a1
AndyGarcia <andygarciaprost2004@gmail.com>
2025-08-05 17:33:27 -0300

    Update: Only Progra I

--- SegundoCuatrimestre/TrabajosPracticos/TpNro9/Ejercicio1Libreria/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from rutas.rutas_libro import libros_bp
-
-app = Flask(__name__)
-# Registrar el "blueprint" en la aplicación
-app.register_blueprint(libros_bp)
-
-if __name__ == '__main__':
-    app.run()
--- SegundoCuatrimestre/TrabajosPracticos/TpNro9/Ejercicio2Biblioteca/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from rutas.rutas_socio import socios_bp
-
-app = Flask(__name__)
-# Registrar el "blueprint" en la aplicación
-app.register_blueprint(socios_bp)
-
-if __name__ == '__main__':
-    app.run()
